1cc40368e69a8c7a29a1cb2e4007d074b05b031b
simonnn <156129760+Simon-Aspee@users.noreply.github.com>
2024-11-18 14:57:16 +0000

    actualizacion grafico

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Ruta al archivo XLS
 url = "https://datos.gob.cl/dataset/84d6e373-10af-41c9-a4d2-e80db6f94e01/resource/e72a5cc2-d0ec-4d8f-b092-637a513f2c5b/download/aeronaves-inscritas-en-el-r.n.a.-al-31.oct.2024-plataforma-del-gobierno.xlsx"
@@ -19,9 +20,24 @@ st.write(database)
 
 # Graficar los datos
 fig, ax = plt.subplots(figsize=(10, 6))
+
 ax.hist(database['TIPO DE AERONAVE'], bins=20, edgecolor='black')
 ax.set_xlabel('Tipo de Aeronave')
 ax.set_ylabel('Frecuencia')
 ax.set_title('Histograma de Tipos de Aeronaves')
 
 st.pyplot(fig)
+
+# Gráfico de pastel
+x = database['TIPO DE AERONAVE'].value_counts()
+colors = plt.get_cmap('Blues')(np.linspace(0.2, 0.7, len(x)))
+
+# plot
+fig, ax = plt.subplots()
+ax.pie(x, colors=colors, radius=3, center=(4, 4),
+       wedgeprops={"linewidth": 1, "edgecolor": "white"}, frame=True)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
